6344d5b16332379effe70c5ef1050f52bd338a9f
Martial Hue <martial.hue@gmail.com>
2025-08-10 13:34:08 +0200

    Remove unused variable

--- colors.py
+++ colors.py
@@ -6,6 +6,3 @@ COLOR_NAMES_FIRST = [
 COLOR_NAMES_SECOND = [
     'red', 'cyan', 'violet', 'yellow', 'pink', 'orange', 'green', 'white', 'blue'
 ]
-
-# Backward compatibility: default palette used by existing code
-COLOR_NAMES = COLOR_NAMES_FIRST
